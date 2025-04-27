5958e01df1f6e524055d60db6e9e2f70cbf9c55c
Cayke Veras <caykedanielcd@gmail.com>
2025-04-27 17:58:24 -0300

    fix: response in professional reports

--- src/professional-reports/professional-reports.service.ts
+++ src/professional-reports/professional-reports.service.ts
@@ -6,15 +6,72 @@ export class ProfessionalReportsService {
   constructor(private prisma: PrismaService) {}
 
   async getProfessionalPatients(professional_id: string) {
-    const ppt = await this.prisma.professionalPatient.findMany({
+    const patients = await this.prisma.professionalPatient.findMany({
       where: { professionalId: professional_id },
-      include: {
-        patient: true,
+      select: {
+        patient: {
+          select: {
+            id: true,
+            name: true,
+            feelings: {
+              select: {
+                emotion: true,
+                createdAt: true,
+              },
+            },
+          },
+        },
+        TherapySession: {
+          select: {
+            startDate: true,
+            endDate: true,
+            done: true,
+            confirmed: true,
+          },
+          orderBy: {
+            startDate: 'asc',
+          },
+        },
       },
     });
-    if (!ppt)
-      throw new NotFoundException('No patients found for this professional.');
-    return ppt;
+
+    const response = patients.map((patientRelation) => {
+      const { patient, TherapySession } = patientRelation;
+      const pastSessions = TherapySession.filter((session) => session.done);
+      const lastSession =
+        pastSessions.length > 0
+          ? pastSessions[pastSessions.length - 1].startDate.toISOString()
+          : '';
+      const futureSessions = TherapySession.filter(
+        (session) => !session.done && session.confirmed,
+      );
+      const nextSession =
+        futureSessions.length > 0
+          ? futureSessions[0].startDate.toISOString()
+          : '';
+      let status = 'active' as const;
+
+      const emotionScore = patient.feelings.length; // this isn't ready
+
+      const trends = patient.feelings
+        .slice(-5)
+        .map((feeling) => feeling.emotion);
+
+      const avatar = `https://ui-avatars.com/api/?name=${encodeURIComponent(patient.name)}&background=random`; // this isn't ready
+
+      return {
+        id: patient.id,
+        name: patient.name,
+        lastSession,
+        nextSession,
+        emotionScore,
+        status,
+        trends,
+        avatar,
+      };
+    });
+
+    return response;
   }
 
   async getProfessionalSignatureStatus(professional_id: string) {
