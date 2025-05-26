4cc86f02785c52cd3ba5c3440a1c70908831537f
Cayke Daniel <contato.cayke@gmail.com>
2025-05-26 18:45:54 -0300

    fix: .env file not working with quotes

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod';
 
+const cleanQuotes = (val: string) => val.replace(/^['"]|['"]$/g, '');
+
 const schema = z.object({
   R2_ENDPOINT: z.string().url(),
   R2_ACCESS_KEY_ID: z.string(),
   R2_SECRET_ACCESS_KEY: z.string(),
   R2_BUCKET: z.string(),
   CDN_URL: z.string().url(),
-  EMAIL_FROM: z.string(),
+  EMAIL_FROM: z.string().transform(cleanQuotes),
   RESEND_API_KEY: z.string(),
   DISCORD_LOG_PROJECT_ID: z.string(),
   DISCORD_LOG_WEBHOOK_URL: z.string().url(),
